ba31464c0367ed632f6a803adbe1f2e0c42d6e9b
nahom zenebe <nahomzenebe123@gmail.com>
2025-08-15 14:49:22 +0300

    first commit

---
+++ db/mongodb.py
@@ -0,0 +1,26 @@
+# app/db/mongo.py
+from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    MONGO_URI: str = "mongodb://localhost:27017"
+    MONGO_DB_NAME: str = "learning_platform"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
+
+client: AsyncIOMotorClient = None
+db = None
+
+async def connect_to_mongo():
+    global client, db
+    client = AsyncIOMotorClient(settings.MONGO_URI)
+    db = client[settings.MONGO_DB_NAME]
+    print("✅ Connected to MongoDB")
+
+async def close_mongo_connection():
+    global client
+    client.close()
+    print("❌ MongoDB connection closed")
