ef823a2e0e3153dd18ae85ad6b50454e65c8c288
nahom zenebe <nahomzenebe123@gmail.com>
2026-09-05 18:25:52 +0300

    fix:mongoose connection

--- db/mongodb.py
+++ db/mongodb.py
@@ -7,7 +7,11 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     MONGODB_URL: str = "mongodb://localhost:27017"
 
-    model_config = {"env_file": ".env", "env_file_encoding": "utf-8"}
+    model_config = {
+        "env_file": ".env",
+        "env_file_encoding": "utf-8",
+        "extra": "ignore",
+    }
 
 
 settings = Settings()
